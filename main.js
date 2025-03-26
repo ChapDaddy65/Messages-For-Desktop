e28394c4a05adb3d4aeea4c6e7d66d0e8cfe9f54
Chap <chapdaddy65@gmail.com>
2025-03-26 18:18:20 -0400

    1.0 release

---
+++ main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    },
+    icon: path.join(__dirname, 'icon.png')
+  });
+
+  // Load Google Messages for Web
+  win.loadURL('https://messages.google.com/web');
+
+  // Open DevTools in development
+  // win.webContents.openDevTools();
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+}); 
